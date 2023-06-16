c9506020f387fa350267cc825f06832c662d9071
ZooMix <salnikovsanya2002@gmail.com>
2023-06-16 23:48:58 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# Вводится 2 числа, найти произведение чисел.
+
+# first_number = int(input('Введите первое число: '))
+# second_number = int(input('Введите второе число: '))
+# print(first_number * second_number)
+
+# Типы данных
+# a = 'привет'
+# print(type(a))
+#
+# b = 4
+# print(type(b))
+#
+# c = 4.34
+# print(type(c))
+#
+# d = True
+# print(type(d))
+
+# a = 5
+# a = 'five'
+# print(a)
+
+# number = int(input('Введите число: '))
+# if number > 0:
+#     if number != 10:
+#         print(f'Число {number} положительное и не равно 10')
+# elif number == 0:
+#     print('Ноль')
+# else:
+#     print(f'Число {number} отрицательное')
+
+# a = 10
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
